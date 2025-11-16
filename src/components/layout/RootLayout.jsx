fc1bfe3f4b5ef2074df34b378a3a03502ee285bb
Shimaa-Abdelmoaty <shimaaabdelmoaty260@gmail.com>
2025-11-16 18:48:51 +0200

    scroll top automatic in page

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import ScrollToTop from "../ScrollToTop";
 
 function RootLayout() {
     return (
         <div className="min-h-screen flex flex-col font-geist">
+            <ScrollToTop />  {/*  Scrolls to top on every route change */}
+
             <Header />
-            <main className="flex-grow">
-                {/* Nested route content renders here */}
+            <main className="grow">
                 <Outlet />
             </main>
             <Footer />
